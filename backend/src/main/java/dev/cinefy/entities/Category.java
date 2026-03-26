3bc7125fe926b4cbed9b469392ce1ebebe681ffe
nosdepjr <edson.ej100@gmail.com>
2026-03-26 12:28:00 -0300

    feat: add Category entity

---
+++ backend/src/main/java/dev/cinefy/entities/Category.java
@@ -0,0 +1,17 @@
+package dev.cinefy.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_category")
+@Getter
+@Setter
+public class Category{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column(length = 100, nullable = false)
+    String name;
+}
