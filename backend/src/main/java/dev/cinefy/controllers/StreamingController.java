8b87fbf85827b29199f392421c6d160d34f7e004
nosdepjr <edson.ej100@gmail.com>
2026-03-28 11:14:21 -0300

    feat: add StreamingController with basic endpoints

---
+++ backend/src/main/java/dev/cinefy/controllers/StreamingController.java
@@ -0,0 +1,54 @@
+package dev.cinefy.controllers;
+
+import dev.cinefy.controllers.request.StreamingRequest;
+import dev.cinefy.controllers.response.StreamingResponse;
+import dev.cinefy.services.StreamingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/streamings")
+@RequiredArgsConstructor
+public class StreamingController {
+
+    private final StreamingService streamingService;
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Hello World!");
+    }
+
+    @PostMapping
+    public ResponseEntity<StreamingResponse> createStreaming(
+            @RequestBody StreamingRequest request) {
+
+        StreamingResponse response = streamingService.createStreaming(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StreamingResponse>> findAllCategories() {
+        return ResponseEntity.ok(streamingService.findAllStreamings());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StreamingResponse> findCategoryById(@PathVariable Long id) {
+        StreamingResponse response = streamingService.findStreamingById(id);
+
+        if (response == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
+        streamingService.deleteStreamingById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- backend/src/main/java/dev/cinefy/repositories/StreamingRepository.java
+++ backend/src/main/java/dev/cinefy/repositories/StreamingRepository.java
@@ -2,6 +2,8 @@ package dev.cinefy.repositories;
 
 import dev.cinefy.entities.Streaming;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StreamingRepository extends JpaRepository<Streaming, Long>{
 }
